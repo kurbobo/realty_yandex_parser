98df16339a310eb1b084cd6e860c32c7b222abf0
kurbobo <kurbobo239@gmail.com>
2019-09-26 00:51:09 +0300

    search on many pages added

--- search_cian.py
+++ search_cian.py
@@ -1,9 +1,18 @@
 from selenium import webdriver
+n = 10
+open('cian.txt','w').close()
 try:
-	element_list = []
-	browser = webdriver.Firefox()
-	browser.get("https://spb.cian.ru/kupit-kvartiru-1-komn-ili-2-komn/")
-	element_list =list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.c6e8ba5398--info-container--A11gU')))
+	with open('cian.txt', 'a', encoding='utf-8') as f:
+		browser = webdriver.Firefox()
+		browser.get("https://spb.cian.ru/kupit-kvartiru-1-komn-ili-2-komn/")
+		for i in range(2,n):
+			# button =browser.find_element_by_css_selector('li._93444fe79c--list-item--2KxXr:nth-child('+str(n)+')')
+			element_list =list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.c6e8ba5398--info-container--A11gU')))
+			for text in element_list:
+				f.write(text)
+			new_window_url = browser.find_element_by_css_selector(':nth-child('+str(i)+') > a._93444fe79c--list-itemLink--3o7_6').get_attribute("href")
+			print(new_window_url)
+			browser.get(new_window_url)
 finally:
 	browser.quit()
-	print(element_list)
+	# print(element_list)
