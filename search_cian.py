74e31bae75d58d2370b8f215a23556110d6c1478
Nikita Sobolev <niksob97@mail.ru>
2019-12-01 17:52:01 +0300

    [Introduce] First version of parallel parser
    Now parser does not try to step over pages. It obtain
    advertisement by it's ID. Introduce option for parallel
    execution of script on multiple cores.
    
    Signed-off-by: Nikita Sobolev <niksob97@mail.ru>

--- search_cian.py
+++ search_cian.py
@@ -15,23 +15,30 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument("no-sandbox")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--headless")
+#chrome_options.add_argument("--headless")
 driver = os.path.join("/usr/local/bin","chromedriver")
 prefs = {'disk-cache-size': 4096}
 chrome_options.add_experimental_option("prefs", prefs)
-info_list = []
 
 # соединяемся с сервером базы данных
 # (по умолчанию подключение осуществляется на localhost:27017)
-connect = pymongo.MongoClient('localhost', 27017)
+connect = pymongo.MongoClient('localhost', 27017, maxPoolSize=200)
 
 # выбираем базу данных
 db = connect.flats
 
 # выбираем коллекцию документов
-coll = db.user
+db.user
+
+
 start = clock()
 
+
+db_free = 1
+
+initial_id = 220833621
+num_of_nodes = 2
+# driver_names = list(map(lambda i: 'browser'))
 
 def parser(flat_string):
 	amount_of_rooms, total_square = amount_and_square_parser(flat_string)
@@ -77,68 +84,75 @@ def parser(flat_string):
 					'total_number_views': total_number_views_parser(flat_string)}
 	return element_dict
 
+def crawler(page_id):
+	try:
+		with open( 'ads_texts/'+ str(page_id) + '.txt', 'a', encoding='utf-8') as output_file:
 
-try:
-	with open('cian.txt', 'a', encoding='utf-8') as output_file:
-		browser = webdriver.Chrome(chrome_options=chrome_options)
-		browser.get("https://spb.cian.ru/kupit-kvartiru-1-komn-ili-2-komn/")
-		for page_num in range(0,number_of_pages):
-			page_num += 1
-			print(page_num)
+			browser = webdriver.Chrome(chrome_options=chrome_options)
+			print('.')
+
 			# Get the URL of next page to be parsed
+			browser.get("https://spb.cian.ru/sale/flat/" + str(page_id) + "/")
+
+			element_list = list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--header--2Ayiz')))
+			element_list += ["address:\n"]
+			element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('address.a10a3f92e9--address--140Ec')))
+			element_list += ["\n"]
+			element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--description--10czU')))
+			element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--price-container--29gwP')))
+			element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--section_divider--1zGrv')))
+			element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--offer_card_page-main--1glTM a10a3f92e9--aside_banner--2FWCV')))
+			element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--offer_card_page-bti--2BrZ7')))
+			element_list += ["\n"]
+			element_list += ["ID_num: " + str(page_id)]
+			element_list += ["\n"]
 			try:
-				new_window_url = browser.find_element_by_xpath('//a[@class="_93444fe79c--list-itemLink--3o7_6" and text()="'+str(i)+'"]').get_attribute("href")
-			except:
-				new_window_url = browser.find_element_by_xpath('//a[@class="_93444fe79c--list-itemLink--3o7_6" and text()=".."]').get_attribute("href")
-
-			# Get URLs of all current ads for parsing
-			ads_on_page = list(map(lambda x: x.get_attribute("href"), browser.find_elements_by_xpath(
-				'//a[@class="link_component-link-xUBVR4w6" and text()="Подробнее"]')))
-			# Open every ad to find more info_dict
-			for page_ad in ads_on_page:
-				browser.get(page_ad)
-				element_list = list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--header--2Ayiz')))
-				element_list += ["address:\n"]
-				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('address.a10a3f92e9--address--140Ec')))
+				browser.find_element_by_css_selector('a.a10a3f92e9--link--1t8n1.a10a3f92e9--link--2mJJk').click()
+				time.sleep(0.5)
+				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--information--AyP9e')))
 				element_list += ["\n"]
-				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--description--10czU')))
-				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--price-container--29gwP')))
-				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--section_divider--1zGrv')))
-				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--offer_card_page-main--1glTM a10a3f92e9--aside_banner--2FWCV')))
-				element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--offer_card_page-bti--2BrZ7')))
-				element_list += ["\n"]
-				element_list += ["ID_num: " + str(page_ad.split('/')[-2])]
-				element_list += ["\n"]
-				try:
-					browser.find_element_by_css_selector('a.a10a3f92e9--link--1t8n1.a10a3f92e9--link--2mJJk').click()
-					time.sleep(0.5)
-					element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector('div.a10a3f92e9--information--AyP9e')))
+				for elementName in browser.find_elements_by_css_selector("path.highcharts-point"):
+					hover = ActionChains(browser).move_to_element(elementName).click().perform()
+					time.sleep(0.1)
+					element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector("g.highcharts-label.highcharts-tooltip.highcharts-color-undefined")))
 					element_list += ["\n"]
-					for elementName in browser.find_elements_by_css_selector("path.highcharts-point"):
-						hover = ActionChains(browser).move_to_element(elementName).click().perform()
-						time.sleep(0.1)
-						element_list += list(map(lambda x: x.text, browser.find_elements_by_css_selector("g.highcharts-label.highcharts-tooltip.highcharts-color-undefined")))
-						element_list += ["\n"]
-				except:
-					print("No info about visitors in ad: " + str(page_ad))
-				element_str = "".join(element_list)
-				for text in element_list:
-					output_file.write(text + '\n')
-				output_file.write('-------------------------------------------------------------------------\n')
-				info_dict = parser(element_str)
-				info_dict.update( {'pic_urls' : list(map(lambda x: x.get_attribute("src"), browser.find_elements_by_css_selector('img.fotorama__img')))})
-				db.coll.insert_one(info_dict)
-				info_list.append(info_dict)
+			except:
+				print("No info about visitors in ad: " + str(page_ad))
+			element_str = "".join(element_list)
+			for text in element_list:
+				output_file.write(text + '\n')
+			output_file.write('-------------------------------------------------------------------------\n')
+			info_dict = parser(element_str)
+			info_dict.update( {'pic_urls' : list(map(lambda x: x.get_attribute("src"), browser.find_elements_by_css_selector('img.fotorama__img')))})
+			# global db_free
+			# while db_free == 0:
+			# 	time.sleep(0.01)
+			# else:
+			# 	db_free = 0
+			# 	db.coll.insert_one(info_dict)
+			# 	db_free = 1
+			# db.close()
+			browser.quit()
+			return 0
 
-			# Open next page with search results
-			browser.get(new_window_url)
-except Exception as e:
-        print("Error has occured in: " + str(page_ad))
-        print(e)
+	except Exception as e:
+		print("Error has occured in: " + str(page_id))
+		print(e)
+		browser.quit()
+		return 1
 
-finally:
-	end = clock()
-	df = pd.DataFrame(info_list)
-	df.to_csv('cian.csv', sep='\t', index=False)
-	print('Parsing done in ' + str(end - start) + ' seconds.')
-	browser.quit()
+if __name__=="__main__":
+	import multiprocessing as mp
+	print("ololo")
+	pool = mp.Pool(processes=2)
+	# for i in range(0, 5):
+	pool.map(crawler, list(i + initial_id for i in range(0, 15)))
+
+
+
+# #finally:
+end = clock()
+# df = pd.DataFrame(info_list)
+# df.to_csv('cian.csv', sep='\t', index=False)
+print('Parsing done in ' + str(end - start) + ' seconds.')
+#browser.quit()
