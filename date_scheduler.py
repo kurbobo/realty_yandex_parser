d44e75895237a6f50b196f36bb6852f0d8723ca4
kurbobo <kurbobo@yandex.ru>
2020-03-10 08:57:23 +0300

    [alex-dev] Added not working date_scheduler parallel

--- date_scheduler.py
+++ date_scheduler.py
@@ -16,14 +16,14 @@ db = myclient.flats
 mycol = db.coll
 past = (datetime.now() - timedelta(days=10)).strftime('%Y-%m-%d')
 print('past = ', past)
-# import multiprocessing as mp
-# num_of_cores = mp.cpu_count()-2
-# print('Start execution with ' + str(num_of_cores) + ' cores.')
-# pool = mp.Pool(num_of_cores)
+import multiprocessing as mp
+num_of_cores = 1#mp.cpu_count()-2
+print('Start execution with ' + str(num_of_cores) + ' cores.')
+pool = mp.Pool(num_of_cores)
 for old_dict in mycol.find({ 'date_of_adding_to_db': {"$lt": past}, 'seen_as_old': { "$exists": False } , 'visitors': {"$ne": None}}):
 	try:
-		# pool.apply_async(updating_crawler, args=(old_dict['id']))
-		updating_crawler(old_dict['id'])
+		pool.apply_async(updating_crawler, args=(old_dict['id']))
+		# updating_crawler(old_dict['id'])
 		# myquery = { "_id":  old_dict['_id']}
 		# newvalues = { "$set": { 'seen_as_old': True } }
 		# mycol.update_one(myquery, newvalues)
@@ -32,6 +32,6 @@ for old_dict in mycol.find({ 'date_of_adding_to_db': {"$lt": past}, 'seen_as_old
 		print('error')
 		myclient.close()
 		break
-# pool.close()
-# pool.join()
+pool.close()
+pool.join()
 myclient.close()
