4a1679aedc396c0640caa89dc898d03c428d9a97
kurbobo <kurbobo@yandex.ru>
2020-03-10 08:54:01 +0300

    [alex-dev] Added function updating of page_id

--- date_scheduler.py
+++ date_scheduler.py
@@ -2,6 +2,13 @@ import pymongo
 from datetime import datetime, timedelta
 from search_cian import crawler
 
+def updating_crawler(page_id):
+	crawler(page_id)
+	myquery = { "_id":  page_id}
+	newvalues = { "$set": { 'seen_as_old': True } }
+	mycol.update_one(myquery, newvalues)
+	print('updated', page_id)
+
 
 myclient = pymongo.MongoClient('localhost', 27017, maxPoolSize=200)
 db = myclient.flats
@@ -9,19 +16,22 @@ db = myclient.flats
 mycol = db.coll
 past = (datetime.now() - timedelta(days=10)).strftime('%Y-%m-%d')
 print('past = ', past)
-i = 0
+# import multiprocessing as mp
+# num_of_cores = mp.cpu_count()-2
+# print('Start execution with ' + str(num_of_cores) + ' cores.')
+# pool = mp.Pool(num_of_cores)
 for old_dict in mycol.find({ 'date_of_adding_to_db': {"$lt": past}, 'seen_as_old': { "$exists": False } , 'visitors': {"$ne": None}}):
-	# try:
-		print('hey', old_dict['id'])
-		crawler(old_dict['id'], i)
-		myquery = { "_id":  old_dict['_id']}
-		newvalues = { "$set": { 'seen_as_old': True } }
-		mycol.update_one(myquery, newvalues)
-		print('hey', old_dict['id'])
-		i+=1
-	# except:
-	# 	print('error')
-	# 	myclient.close()
-	# 	break
-		
+	try:
+		# pool.apply_async(updating_crawler, args=(old_dict['id']))
+		updating_crawler(old_dict['id'])
+		# myquery = { "_id":  old_dict['_id']}
+		# newvalues = { "$set": { 'seen_as_old': True } }
+		# mycol.update_one(myquery, newvalues)
+		# print('updated', old_dict['id'])
+	except:
+		print('error')
+		myclient.close()
+		break
+# pool.close()
+# pool.join()
 myclient.close()
