5dced7702e6d828b768b6d38a4c84e1339386960
kurbobo <kurbobo@yandex.ru>
2020-03-13 15:17:31 +0300

    [alex-dev] Fixed bug in date_scheduler
    The program didn't add any kind of changings in seen_as_old ads.

--- date_scheduler.py
+++ date_scheduler.py
@@ -2,9 +2,9 @@ import pymongo
 from datetime import datetime, timedelta
 from search_cian import crawler
 
-def updating_crawler(page_id):
-	crawler(page_id)
-	myquery = { "_id":  page_id}
+def updating_crawler(page_id, mongo_id):
+	crawler(page_id, stop_trying_treshhold=100)
+	myquery = { "_id":  mongo_id}
 	newvalues = { "$set": { 'seen_as_old': True } }
 	mycol.update_one(myquery, newvalues)
 	print('updated', page_id)
@@ -17,17 +17,13 @@ mycol = db.coll
 past = (datetime.now() - timedelta(days=10)).strftime('%Y-%m-%d')
 print('past = ', past)
 import multiprocessing as mp
-num_of_cores = 1#mp.cpu_count()-2
+num_of_cores = mp.cpu_count()-2
 print('Start execution with ' + str(num_of_cores) + ' cores.')
 pool = mp.Pool(num_of_cores)
 for old_dict in mycol.find({ 'date_of_adding_to_db': {"$lt": past}, 'seen_as_old': { "$exists": False } , 'visitors': {"$ne": None}}):
 	try:
-		pool.apply_async(updating_crawler, args=(old_dict['id']))
-		# updating_crawler(old_dict['id'])
-		# myquery = { "_id":  old_dict['_id']}
-		# newvalues = { "$set": { 'seen_as_old': True } }
-		# mycol.update_one(myquery, newvalues)
-		# print('updated', old_dict['id'])
+		pool.apply_async(updating_crawler, args=(old_dict['id'], old_dict['_id']))
+		# updating_crawler(old_dict['id'], old_dict['_id'])
 	except:
 		print('error')
 		myclient.close()
