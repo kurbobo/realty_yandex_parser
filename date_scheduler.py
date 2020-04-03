6b22ef31147f5d411f36f90b948b68484aa78472
kurbobo <kurbobo@yandex.ru>
2020-04-03 17:29:55 +0300

    [alex-dev] Added number of tries option in date scheduler

--- date_scheduler.py
+++ date_scheduler.py
@@ -2,8 +2,8 @@ import pymongo
 from datetime import datetime, timedelta
 from search_cian import crawler
 import argparse
-def updating_crawler(page_id, mongo_id):
-	crawler(page_id, stop_trying_treshhold=100)
+def updating_crawler(page_id, mongo_id, stop_trying_treshhold):
+	crawler(page_id, stop_trying_treshhold)
 	myquery = { "_id":  mongo_id}
 	newvalues = { "$set": { 'seen_as_old': True } }
 	mycol.update_one(myquery, newvalues)
@@ -12,6 +12,7 @@ def updating_crawler(page_id, mongo_id):
 if __name__=="__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-v", "--only_visitors", help="update ads only for ads with visitor's statistics", action='store_true')
+	parser.add_argument("-n", "--number_of_tries", help="how many attempts for every ad to make", default=100)
 	args = parser.parse_args()
 	myclient = pymongo.MongoClient('localhost', 27017, maxPoolSize=200)
 	db = myclient.flats
@@ -28,7 +29,7 @@ if __name__=="__main__":
 		dct['visitors'] = {"$ne": None}
 	for old_dict in mycol.find(dct):
 		try:
-			pool.apply_async(updating_crawler, args=(old_dict['id'], old_dict['_id']))
+			pool.apply_async(updating_crawler, args=(old_dict['id'], old_dict['_id'], args.number_of_tries))
 			# updating_crawler(old_dict['id'], old_dict['_id'])
 		except:
 			print('error')
