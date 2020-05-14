693d4195795bf6ff5061f681a9a38999f41a6183
kurbobo <kurbobo@yandex.ru>
2020-05-14 09:46:29 +0300

    it was used multiprocessing in fixing of repair type

--- fix_problems/fix_problem_with_missed_repair_type.py
+++ fix_problems/fix_problem_with_missed_repair_type.py
@@ -1,25 +1,36 @@
+import time
 import os
 import sys
 sys.path.insert(1, '../search')
 import parser_tools as pt
 import pymongo
+from multiprocessing.dummy import Pool as ThreadPool 
+from multiprocessing import cpu_count
+
+
 os.chdir('../ads_texts')
 ads_list = os.listdir()
 # соединяемся с сервером базы данных
 # (по умолчанию подключение осуществляется на localhost:27017)
 connect = pymongo.MongoClient('localhost', 27017, maxPoolSize=200)
-# выбираем базу данных
-db = connect.flats
 # выбираем коллекцию документов
-# db.coll
-import tqdm
-for ad in tqdm.tqdm(ads_list):
+
+coll = connect.flats.coll
+thread_pool_size = cpu_count()
+pool = ThreadPool(thread_pool_size) 
+
+def my_function(ad):
 	with open(ad, 'r') as ad:
-				x = ''.join(ad.readlines())
-				repair =  pt.repair_type_parser(x)
-				if repair is not None:
-					# print('updated')
-					myquery = { "id": int(ad.name[:-4]), "repair_type": None}
-					newvalues = { "$set": { "repair_type": repair } }
-					db.coll.update_many(myquery, newvalues)
+		x = ''.join(ad.readlines())
+		repair =  pt.repair_type_parser(x)
+		if repair is not None:
+			# print('updated')
+			myquery = { "id": int(ad.name[:-4]), "repair_type": None}
+			newvalues = { "$set": { "repair_type": repair } }
+			coll.update_many(myquery, newvalues)
+start = time.time()
+pool.map(my_function, ads_list)
+
+print('ended in ', time.time()-start)
+
 connect.close()
