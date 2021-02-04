af169711f8db4ac0f4674ba6e4b4c861a28f096d
kurbobo <kurbobo@yandex.ru>
2021-02-04 13:25:19 +0300

    [debug] Change of function crawler to class

--- search/search_cian_asyncio.py
+++ search/search_cian_asyncio.py
@@ -10,16 +10,21 @@ import subprocess
 import multiprocessing as mp
 from collections import Counter
     
-def crawler(page_id, tbb_dir=None, stop_trying_treshhold=12, loop=None):
-    # print('start crawler')
-    time.sleep(1*random.random())
-    if page_id not in MyGlobals.state_dict.keys():
-        MyGlobals.state_dict[page_id] = None
-    # download_data(page_id, tbb_dir)
-    loop.run_in_executor(executor, download_data, page_id, tbb_dir)
-    print('Ad with number: ' + str(page_id) + ' finished parsing.')
-    
-    ''' increment the global counter, do something with the input '''
+class Crawler(object):
+    """
+    class for crawing cian
+    :param page_id: integer, must be positive. Describes the id of the page, from which
+    we should start crawling
+    :param tbb_dir: string, the path to the tor-browser
+    :param stop_trying_treshhold: integer, must be positive. The number of attempts to parse single page
+    :return: integer
+    """
+    def __call__(self, page_id, tbb_dir=None, stop_trying_treshhold=12, loop=None):
+        time.sleep(1*random.random())
+        if page_id not in MyGlobals.state_dict.keys():
+            MyGlobals.state_dict[page_id] = None
+        loop.run_in_executor(executor, download_data, page_id, tbb_dir)
+        print('Ad with number: ' + str(page_id) + ' finished parsing.')
 if __name__=="__main__":
     #for automatic getting of current initial_id
     initial_id = int(subprocess.check_output(["./get_last_ad.sh"]))
@@ -31,6 +36,7 @@ if __name__=="__main__":
     for ad in range(N):#100000
         if ad>=N-10:
             break
+        crawler = Crawler()
         crawler(initial_id + ad, tbb_dir, loop=loop)
     for _ in range(4):
         print('Counter(MyGlobals.state_dict.values())[None] is ', Counter(MyGlobals.state_dict.values())[None])
